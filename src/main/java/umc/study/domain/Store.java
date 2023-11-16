330052bd8f0cdc461ac7b5dd9a87c0d6133c28f1
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-16 19:51:43 +0900

    feat: update Store
    - toEntity 생성
    - 리뷰리스트 속성 추가

--- src/main/java/umc/study/domain/Store.java
+++ src/main/java/umc/study/domain/Store.java
@@ -2,6 +2,8 @@ package umc.study.domain;
 
 import lombok.*;
 import umc.study.domain.common.BaseEntity;
+import umc.study.web.dto.review.ReviewRequestDto;
+import umc.study.web.dto.store.StoreRequestDto;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -30,4 +32,15 @@ public class Store extends BaseEntity {
 
     @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
     private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    public static Store toEntity(StoreRequestDto requestDto){
+        return Store.builder()
+                .name(requestDto.getName())
+                .address(requestDto.getAddress())
+                .score(requestDto.getScore())
+                .build();
+    }
 }
