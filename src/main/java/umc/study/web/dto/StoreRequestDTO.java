acaaa43e133859c76bf9218a34a314dcb370ce48
윤호 남 <nam2660433@gmail.com>
2023-11-17 22:49:42 +0900

    feat: 특정 지역에 가게 추가하기 API Validator 추가

--- src/main/java/umc/study/web/dto/StoreRequestDTO.java
+++ src/main/java/umc/study/web/dto/StoreRequestDTO.java
@@ -1,13 +1,19 @@
 package umc.study.web.dto;
 
 import lombok.Getter;
+import umc.study.validation.annotation.ExistRegion;
+
+import javax.validation.constraints.NotBlank;
 
 public class StoreRequestDTO {
 
     @Getter
     public static class RegionStoreDTO{
+        @ExistRegion
         Long regionId;
+        @NotBlank
         String name;
+        @NotBlank
         String address;
         Float score;
     }
