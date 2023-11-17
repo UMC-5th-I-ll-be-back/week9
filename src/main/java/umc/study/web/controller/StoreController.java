6982409b66abea3c4309b467a6cd2f7051ad290f
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-17 13:31:55 +0900

    feat: add 수정 및 get 추가

--- src/main/java/umc/study/web/controller/StoreController.java
+++ src/main/java/umc/study/web/controller/StoreController.java
@@ -1,16 +1,16 @@
 package umc.study.web.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.study.domain.Store;
 import umc.study.service.StoreService.StoreService;
 import umc.study.web.dto.store.StoreRequestDto;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +20,18 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping
-    public ResponseEntity<Store> addMission(@RequestBody StoreRequestDto storeRequestDto){
-        Store savedStore = storeService.addStore(storeRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedStore);
+    public ResponseEntity<Object> addStore(@RequestBody StoreRequestDto storeRequestDto){
+        storeService.addStore(storeRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getStoreList() {
+        return new ResponseEntity<>(storeService.getStoreList(),HttpStatus.FOUND);
+    }
+
+    @GetMapping("/{store_id}")
+    public ResponseEntity<?> getStoreReviewList(@PathVariable Long store_id){
+        return new ResponseEntity<>(storeService.getStoreReviewList(store_id), HttpStatus.FOUND);
     }
 }
