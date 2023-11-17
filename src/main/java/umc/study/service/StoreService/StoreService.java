ed63e6c6564b081f6857b96d6fb3cfaa2579f5fd
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-17 13:27:10 +0900

    feat: 조회 로직 추가
    - getStoreList
    - getStoreReviewList(Long id): id로 가게에 해당된 리뷰조회

--- src/main/java/umc/study/service/StoreService/StoreService.java
+++ src/main/java/umc/study/service/StoreService/StoreService.java
@@ -2,16 +2,41 @@ package umc.study.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import umc.study.domain.Review;
 import umc.study.domain.Store;
 import umc.study.repository.StoreRepository;
 import umc.study.web.dto.store.StoreRequestDto;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class StoreService {
-    private StoreRepository storeRepository;
+    private final StoreRepository storeRepository;
 
-    public Store addStore(StoreRequestDto requestDto){
-        return storeRepository.save(Store.toEntity(requestDto));
+    @Transactional
+    public void addStore(StoreRequestDto requestDto)
+    {
+        Store store = storeRepository.findByName(requestDto.getName());
+        if(store == null){
+            storeRepository.save(Store.toEntity(requestDto));
+        }
+        else{
+            // TODO: 2023/11/17 현재는 중복되는 가게 이름으로 체크 -> 상의 후 로직변경(ex. 가게이름+지역 조합으로 구분하기 or 중복허용하기 or 토큰으로 구분)
+            throw new IllegalArgumentException("이미 있는 가게입니다.");
+        }
     }
+
+    public List<Store> getStoreList(){
+        return storeRepository.findAll();
+    }
+
+    public List<Review> getStoreReviewList(Long id){
+        Optional<Store> store = storeRepository.findById(id);
+        List<Review> reviewList = store.get().getReviewList();
+        return reviewList;
+    }
+
 }
