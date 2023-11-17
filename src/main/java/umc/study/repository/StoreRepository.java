47b2df5e40140ec82a05f4107482061803daf797
윤호 남 <nam2660433@gmail.com>
2023-11-17 23:33:36 +0900

    feat: 특정 지역에 가게 추가하기 API .

--- src/main/java/umc/study/repository/StoreRepository.java
+++ src/main/java/umc/study/repository/StoreRepository.java
@@ -3,5 +3,9 @@ package umc.study.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.Store;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Store findByName(String name);
+    Optional<Store> findById(Long id);
 }
