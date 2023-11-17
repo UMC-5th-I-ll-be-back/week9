f6910139343a1563da1f6debb37a8767dd3a0cb5
tjdgns8439 <tjdgns8439@inha.edu>
2023-11-17 13:20:08 +0900

    feat: update StoreRepository
    - id로 Store 찾아오기

--- src/main/java/umc/study/repository/StoreRepository.java
+++ src/main/java/umc/study/repository/StoreRepository.java
@@ -3,6 +3,10 @@ package umc.study.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.Store;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
     Store findByName(String name);
+    Optional<Store> findById(Long id);
 }
